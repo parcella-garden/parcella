52cc04b1af4cef74b9cc61e3084877add3e3322e
kermie <1337898+kermie@users.noreply.github.com>
2026-08-18 22:58:11 +0200

    Bump app_version to 1.0.5

--- app/config.py
+++ app/config.py
@@ -32,7 +32,7 @@ class Settings(BaseSettings):
 
     # App metadata
     app_name: str = "Parcella"
-    app_version: str = "1.0.4"
+    app_version: str = "1.0.5"
 
     @property
     def is_development(self) -> bool:
